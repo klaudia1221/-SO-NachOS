34fb55e472652e05fbed0e0c42ce8033643c2d9b
msciborski <michal.sciborski@student.put.poznan.pl>
2018-01-02 19:12:53 +0100

    Fixing FileSystem

--- src/com/BamoOS/Modules/FileSystem/IFileSystem.java
+++ src/com/BamoOS/Modules/FileSystem/IFileSystem.java
@@ -4,14 +4,14 @@ import com.BamoOS.Modules.ACL.User;
 
 public interface IFileSystem {
     FileBase getFileBase(String name);
-    int openFile(String fileName);
-    int closeFile(String name);
-    int createFile(String fileName, User user);
-    int appendFile(String fileName, String content);
-    int deleteContent(String fileName);
-    String readFile(String fileName);
-    int deleteFile(String fileName);
-    int renameFile(String oldName, String newName);
+    void openFile(String fileName) throws Exception;
+    void closeFile(String name) throws Exception;
+    void createFile(String fileName, User user) throws Exception;
+    void appendFile(String fileName, String content) throws Exception;
+    void deleteContent(String fileName) throws Exception;
+    String readFile(String fileName) throws Exception;
+    void deleteFile(String fileName) throws Exception;
+    void renameFile(String oldName, String newName) throws Exception;
     String list();
 
 }
