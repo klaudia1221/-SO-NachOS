30f3eec35e357ae50177a8636d06337d5fc6de09
msciborski <michal.sciborski@student.put.poznan.pl>
2018-01-02 12:50:20 +0100

    Changes in interpreter

---
+++ src/com/BamoOS/Modules/Interpreter/IInterpreter.java
@@ -0,0 +1,16 @@
+package com.BamoOS.Modules.Interpreter;
+
+public interface IInterpreter {
+    void set_A();
+    void set_B();
+    void set_C();
+    void set_PC();
+    void set_PID();
+    int get_A();
+    int get_B();
+    int get_C();
+    int get_PC();
+    int get_PID();
+    void RegisterStatus();
+    boolean Exe(String[] order);
+}
