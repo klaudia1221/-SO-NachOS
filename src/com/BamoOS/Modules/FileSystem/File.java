22f3e41d6c6580ebd128a280d8f3c4c1f33c7cca
m-wlodarczyk <mich.j.wlodarczyk@gmail.com>
2018-01-03 20:48:15 +0100

    FileSystem found bugs fixed.

--- src/com/BamoOS/Modules/FileSystem/File.java
+++ src/com/BamoOS/Modules/FileSystem/File.java
@@ -12,9 +12,9 @@ public class File extends FileBase{
     public int FILE_SIZE;		//Rozmiar pliku
     int FIRST_BLOCK;			//Pierwszy blok zajmowany przez plik
     int LAST_BLOCK;
+    int BLOCK_NUM;
     String opened_file;
     boolean opened=false;
-    public IConditionVariable cv;
 
     public void setName(String fileName) { this.FILE_NAME = fileName; }
 
@@ -22,9 +22,9 @@ public class File extends FileBase{
 
     public void setLast(int block) { this.LAST_BLOCK = block; }
 
-    public void open(String fileContent) {
-        this.opened_file+=fileContent; opened=true;
-    }
+    public void open(String fileContent) { this.opened_file+=fileContent; opened=true; }
+
+    public void update(String fileContent) { this.opened_file+=fileContent; }
 
     public void close() { opened=false; this.opened_file = new String(); }
 
@@ -37,15 +37,12 @@ public class File extends FileBase{
         this.LAST_BLOCK=33;
     }*/
 
-    public File(String name, int first, User user, IProcessManager processManager){
-        super(user);
+    public File(String name, int first){
         this.FILE_NAME=name;
         this.FILE_SIZE=0;
+        this.BLOCK_NUM=1;
         this.FIRST_BLOCK=first;
         this.LAST_BLOCK=first;
-        this.cv = new ConditionVariable(processManager);
-        //Pytanie do michała, dlaczego to jest kalasa String,
-        // a nie sam string
         this.opened_file=new String();
     }
 
@@ -54,5 +51,4 @@ public class File extends FileBase{
     public String getCon(){
         return this.opened_file;
     }
-
 }
