fb44dca4bb94c2135b0dc280b6f6fbc64de0e269
Jakub <kubasmieszny@gmail.com>
2018-01-03 12:12:47 +0100

    //Kuba S.

--- src/com/BamoOS/Modules/Communication/IPC.java
+++ src/com/BamoOS/Modules/Communication/IPC.java
@@ -34,31 +34,45 @@ public class IPC
         if(pm.checkIfProcessExists(recID)==null) //jesli ID procesu o indeksie i == recID
         {
             //zapisz w odpowiednim polu PCB danego procesu wiadomość
+            ArrayList<Sms> temp_list = pm.getActivePCB().getSmsList();
+            temp_list.add(sms);
+            pm.getActivePCB().setSmsList(temp_list);
+
             //zapisz wiadomosc w globalnym kontenerze wszystich wiadomości
+            allMessages.add(sms);
+
             //powiadom proces-odbiorcę o wiadomości metodą signal()
             System.out.println("Wyslano wiadomosc o tresci "+sms.get_mes()+" do procesu o ID "+recID);
             return;
         }
+
         System.out.println("W tej grupie nie znaleziono procesu o ID "+recID);
     }
 
     public void RM(int senID, Sms sms)
     {
-        if()//kontener wiadomości z PCB jest pusty
+        ArrayList<Sms> temp_list = pm.getActivePCB().getSmsList();
+        if(temp_list.size()==0)//kontener wiadomości z PCB jest pusty
         {
             //przechodzi w stan waiting
         }else
         {
             //wyświetla pierwszą wiadomość, którą znajdzie w kontenerze wiadomości w PCB
+            System.out.println(temp_list.get(0).get_mes());
+
             //usuwa z kontenera w PCB pierwszą wiadomość
+            temp_list.remove(0);
         }
     }
 
     public void display_all()
     {
-        for()//przegląda cały kontener wiadomości z PCB
+        //Sms sms;
+        for(Sms sms : allMessages)//przegląda cały kontener wiadomości z grupy procesow, historie
         {
             //wyświetla wiadomości
+            //sms = allMessages.get(i);
+            System.out.println("ID nadawcy: "+sms.get_senID()+"; ID odbiorcy: "+sms.get_recID()+"; tresc: "+sms.get_mes());
         }
     }
 }
