bd34d7baac815f041246cf921cc36ce9c7dcd47e
msciborski <michal.sciborski@student.put.poznan.pl>
2018-01-03 17:08:02 +0100

    Rewriting Processor //m.sciborski

--- src/com/BamoOS/Modules/Processor/IProcessor.java
+++ src/com/BamoOS/Modules/Processor/IProcessor.java
@@ -1,7 +1,6 @@
 package com.BamoOS.Modules.Processor;
 
 public interface IProcessor {
-	public void dodaj_proces();
-	public void Scheduler();
-	public void wykonaj(String order[]) ;
+	void Scheduler();
+	void exe() ;
 }
