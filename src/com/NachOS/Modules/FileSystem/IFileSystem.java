13a11868ac21e9a1b59da00ed7f6d92667690565
m-wlodarczyk <mich.j.wlodarczyk@gmail.com>
2018-01-10 19:12:18 +0100

    Adding FileSystem Exceptions.

--- src/com/NachOS/Modules/FileSystem/IFileSystem.java
+++ src/com/NachOS/Modules/FileSystem/IFileSystem.java
@@ -3,19 +3,20 @@ package com.NachOS.Modules.FileSystem;
 import com.NachOS.Modules.ACL.User;
 import com.NachOS.Modules.Exceptions.ChangedToWaitingException;
 import com.NachOS.Modules.ProcessManager.IProcessManager;
+import com.NachOS.Modules.Exceptions.*;
 
 public interface IFileSystem {
-    FileBase getFileBase(String name) throws Exception;
-    void openFile(String fileName) throws Exception;
-    void closeFile(String name) throws Exception;
-    void createFile(String fileName, User user, IProcessManager processManager) throws Exception;
-    void appendFile(String fileName, String content) throws Exception;
-    void deleteContent(String fileName) throws Exception;
-    String readFile(String fileName) throws Exception;
-    String readFileShell(String fileName) throws Exception;
-    void deleteFile(String fileName) throws Exception;
-    void renameFile(String oldName, String newName) throws Exception;
-    File getFile(String fileName) throws Exception;
+    FileBase getFileBase(String name) throws FileNameException;
+    void openFile(String fileName) throws FileNameException, ChangedToWaitingException;
+    void closeFile(String name) throws FileNameException, FileNotOpenException;
+    void createFile(String fileName, User user, IProcessManager processManager) throws FileNameException, FileSizeException;
+    void appendFile(String fileName, String content) throws FileNameException, FileNotOpenException, FileSizeException;
+    void deleteContent(String fileName) throws FileNameException, FileNotOpenException;
+    String readFile(String fileName) throws FileNameException, FileNotOpenException;
+    String readFileShell(String fileName) throws FileNameException;
+    void deleteFile(String fileName) throws FileNameException;
+    void renameFile(String oldName, String newName) throws FileNameException;
+    File getFile(String fileName) throws FileNameException;
     String list();
     Catalog getCatalog();
 
