5baaf7b90e34fea4d236055df8d20ac4b067852b
Kamila Urbaniak <kamila.urbaniak26@gmail.com>
2018-01-10 19:01:17 +0100

    getSafe

--- src/com/NachOS/Modules/Interpreter/Interpreter.java
+++ src/com/NachOS/Modules/Interpreter/Interpreter.java
@@ -632,13 +632,13 @@ public class Interpreter implements IInterpreter {
 
         switch (reg){
             case "A":
-                    A = Integer.parseInt(processManager.getCommand(Address));
+                    A = Integer.parseInt(processManager.getSafeMemory(Address));
                 break;
             case "B":
-                    B = Integer.parseInt(processManager.getCommand(Address));
+                    B = Integer.parseInt(processManager.getSafeMemory(Address));
                     break;
                 case "C":
-                    C = Integer.parseInt(processManager.getCommand(Address));
+                    C = Integer.parseInt(processManager.getSafeMemory(Address));
                     break;
                 default:
                     throw new IncorrectRegisterException("Nieprawidlowy rejestr");
